8f14404931014078565d75e75c90a8da3ab97115
hsozbek <hselcukozbek@hotmail.com>
2021-01-17 22:09:04 +0300

    deneme

---
+++ Donguler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (string dizi in diziler)
+            {
+                
+            }
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do Not repeat yourself 
+            //deger tutucu, alias
+            //string kategoriEtiketi = "Kategori";
+            //bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+        }
+    }
+}
