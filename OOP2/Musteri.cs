9c23ad043fb95f5eb69dadb5b73381dd29dc2846
hsozbek <hselcukozbek@hotmail.com>
2021-01-23 23:51:55 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        // Farklı özellikteki nesneler aynı class da yer almaz ayrılması gerek
+
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+    }
